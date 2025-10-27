ecafb052d05436b2be69e2aa64a9c88db488bcc3
pateltee14-dotcom <pateltee14@gmail.com>
2025-10-27 16:59:58 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,22 +4,16 @@ import yt_dlp
 
 app = FastAPI()
 
-# Request ka model
 class DownloadRequest(BaseModel):
     youtube_url: str
 
-@app.get("/")
-def home():
-    return {"message": "YouTube Downloader API Ready!"}
-
-# Main download endpoint
 @app.post("/download")
 async def download_video(req: DownloadRequest):
     try:
         url = req.youtube_url
         ydl_opts = {
             "format": "best",
-            "cookiefile": "cookies.txt"   # yahi name/upload hai to work karega
+            "cookiefile": "cookies.txt"
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=False)
